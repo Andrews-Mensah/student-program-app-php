eaa5feca457293e1d663dbd906dea8eb20684a5b
Jacqueline Abban Botchway <jacqueline.botchway@amalitech.org>
2022-12-19 16:51:20 +0000

    created student database, controllers for fetching all, adding and fectching a specific student

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    return Student::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'first_name'=>'required',
+            'last_name'=>'required',
+            'gender'=>'required',
+            'email'=>'required',
+            'program_id'=>'required'
+        ]);
+        return Student::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $student=Student::find($id);
+        if (!$student) {
+            return 'Student not found';
+        };
+
+        return $student;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CoursesController;
 use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,6 +29,11 @@ Route::get('/courses/search-course/{name}', [CoursesController::class, 'search']
 //Programs Routes
 Route::get('/programs/get-programs', [ProgramController::class, 'index']);
 Route::post('/programs/add-program', [ProgramController::class, 'store']);
+
+//Students Routes
+Route::get('/students/get-student', [StudentController::class, 'index']);
+Route::post('/students/add-student', [StudentController::class, 'store']);
+Route::get('/students/get-student/{id}', [StudentController::class, 'show']);
 
 // Route::resource('courses', CoursesController::class);
 
